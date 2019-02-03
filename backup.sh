cf8b159a77f1f8611b1e21677648cf3dba1db87d
Hypriot Pirate <pi@rpi3.localdomain>
2019-02-03 14:06:51 +0100

    fix permissions

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 type ./pre-backup.sh && ./pre-backup.sh
-rsync -av --delete --exclude 'web/vendor' --exclude 'node_modules' /input/* /mnt/webdav/$output
+rsync -av --delete --exclude 'web/vendor' --exclude 'node_modules' --no-owner --no-group /input/* /mnt/webdav/$output
 echo $(date)
